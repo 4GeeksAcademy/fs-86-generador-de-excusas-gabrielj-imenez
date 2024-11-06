7cf8eeac2be4863d016e03f9939d788495568ef5
gabriel jimenez <gabo0813ajm@gmail.com>
2024-11-06 23:44:52 +0000

    termine el generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,47 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function generadorDeExcusas() {
+    const who = obtenerWhoAleatoria();
+    const action = obtenerActionAleatoria();
+    const what = obtenerWhatAleatoria();
+    const when = obtenerWhenAleatoria();
+    const excusa = generarExcusa(who, action, what, when);
+    return excusa;
+  }
+  function obtenerWhoAleatoria() {
+    const tiposDeWho = new Array(
+      "The dog",
+      "My grandma",
+      "The mailman",
+      "My bird"
+    );
+    const whoPosition = Math.floor(Math.random() * tiposDeWho.length);
+    return tiposDeWho[whoPosition];
+  }
+  function obtenerActionAleatoria() {
+    const tiposDeAction = new Array("ate", "peed", "crushed", "broke");
+    const actionPosition = Math.floor(Math.random() * tiposDeAction.length);
+    return tiposDeAction[actionPosition];
+  }
+  function obtenerWhatAleatoria() {
+    const tiposDeWhat = new Array("my homework", "my phone", "the car");
+    const whatPosition = Math.floor(Math.random() * tiposDeWhat.length);
+    return tiposDeWhat[whatPosition];
+  }
+  function obtenerWhenAleatoria() {
+    const tiposDeWhen = new Array(
+      "this morning",
+      "yesterday",
+      "last night",
+      "just now"
+    );
+    const whenPosition = Math.floor(Math.random() * tiposDeWhen.length);
+    return tiposDeWhen[whenPosition];
+  }
+  function generarExcusa(who, action, what, when) {
+    return `${who} ${action} ${what} ${when}.`;
+  }
+  document.getElementById("excuses").innerHTML = generadorDeExcusas();
   console.log("Hello Rigo from the console!");
 };
